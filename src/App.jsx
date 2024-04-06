915779b1bb1c62b49e60f6a63d54275667db44a3
ariyaltharun <ariyaltharun@gmail.com>
2024-04-06 01:29:43 +0000

    Create simple front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,93 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [creditCardNo, setCreditCardNo] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [amtToTransfer, setAmtToTransfer] = useState("");
+  const [recieverAccNo, setRecieverAccNo] = useState("");
+  const [transactionStatus, setTransactionStatus] = useState(null);
+
+  const transactionBlock = useRef(null);
+
+  const transactHandler = async () => {
+    // If required, add validity check 
+    // if invalid then raise alert and ask user to make it valid
+
+    const data = {
+      creditCardNo: creditCardNo,
+      expiryDate: expiryDate,
+      cvv: cvv,
+      amtToTransfer: amtToTransfer,
+      recieverAccNo: recieverAccNo
+    }
+    let response = await fetch("http://localhost:3000/transact", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    
+    let res = await response.json()   // tbd 
+    if (res.transactionStatus === 'Sucess') {
+      setTransactionStatus("Transaction Succeded");
+      transactionBlock.current.style.backgroundColor = 'green';
+    } else if (res.transactionStatus === 'Fail') {
+      setTransactionStatus("Transaction Failed");
+      transactionBlock.current.style.backgroundColor = 'red';
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <label>Credit Card No.:</label>
+        <input 
+          name='creditCardNo' 
+          type='text'
+          value={creditCardNo}
+          onChange={(e) => setCreditCardNo(e.target.value)} />
+        <br />
+        <label>Expiry Date:</label>
+        <input 
+          name='expiryDate' 
+          type='text'
+          value={expiryDate}
+          onChange={(e) => setExpiryDate(e.target.value)} />
+        <br />
+        <label>CVV No.:</label>
+        <input
+          name='cvv'
+          type='password'
+          value={cvv}
+          onChange={(e) => setCvv(e.target.value)}/>
+        <br />
+        <label>Amount To Transfer:</label>
+        <input
+          name='amtToTransfer'
+          type='text'
+          value={amtToTransfer}
+          onChange={(e) => setAmtToTransfer(e.target.value)}/>
+        <br />
+        <label>Reciever Acc No.:</label>
+        <input
+          name='recieverAccNo'
+          type='text'
+          value={recieverAccNo}
+          onChange={(e) => setRecieverAccNo(e.target.value)}/>
+        <br />     
+
+        <button onClick={transactHandler}>Send Money</button>
+
+        <div style={{'width': 100, 'height': 100, 'backgroundColor': 'green'}} ref={transactionBlock}>
+
+        </div>
+        <h1>{transactionStatus}</h1>
     </>
+
+
   )
 }
 
